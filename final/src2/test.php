3b29e9e6cf4faa62544721bee6dfbaa743755110
trismi <trismi@gmail.com>
2013-03-13 19:55:01 +0000

    updating test code

--- final/src2/test.php
+++ final/src2/test.php
@@ -17,10 +17,7 @@ $model->set_client_id(1);
 // set state to "generate" which returns module_list, shell_list, client_id
 $model->set_state("generate");
 
-
-
-?>
-<html>
+$html = "<html>
 <head>
 <style>
 	ol li
@@ -29,31 +26,31 @@ $model->set_state("generate");
 		margin:10px 0;	
 	}
 </style>
-<script type="application/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-<script type="text/javascript">
+<script type='application/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js'></script>
+<script type='text/javascript'>
 	$( function()
 		{
-			$("ol li").click(
+			$('ol li').click(
 				function()
 				{
-					$("<li class='"+$(this).attr("class")+"'>" + $(this).text() + "</li>").appendTo("#list");
-					$("#list li").click(
+					$(\"<li class='\"+$(this).attr(\"class\")+\"'>\" + $(this).text() + \"</li>\").appendTo(\"#list\");
+					$(\"#list li\").click(
 						function()
 						{
 							
-							$(this).appendTo("#nowhere");	
+							$(this).appendTo(\"#nowhere\");	
 						}
 					);
 
 				}
 			);
 			
-			$("#submit").click(
+			$(\"#submit\").click(
 				function()
 				{
 					
-					alert( $("#list li").text()  );
-					$("form").submit();	
+					alert( $(\"#list li\").text()  );
+					$(\"form\").submit();	
 				}
 			);
 		}
@@ -62,32 +59,31 @@ $model->set_state("generate");
 </head>
 <body>
 
-<form action="action.php" method="post">
-<?php 
+<form action='index.php' method='post'>
+";
 
 // get result array
 $result =  $model->get_templates("shell");
 
 // echo categories
-echo "Pick your shell: <select name=\"category\">"; 
+$html .= "Pick your shell: <select name='category'>"; 
 
 foreach($result as $catname)
 {        
-	echo "<option value='".$catname['module_id']."'>".$catname['name']."</option>"; 
+	$html .= "<option value='".$catname['module_id']."'>".$catname['name']."</option>"; 
 }
-echo "</select>";
+$html.= "</select>";
 
-?>
-
-<div>
+$html.= 
+"<div>
 	<h4>Click the below modules to add them to your email.</h4>
-	<div style="float:left; width:300px;">
+	<div style='float:left; width:300px;'>
         Email Modules
-        <ul id="list"></ul>
+        <ul id='list'></ul>
     
     </div>
+";
 
-<?php
 
 // get result array
 $result =  $model->get_templates("module");
@@ -99,32 +95,32 @@ $newArr = array("hero" => array(), "rescue" => array(), "modules" => array()  );
 foreach($result as $entry)
 {
 			$newArr[ $entry['category'] ][] = $entry;
-			
+
 }
 
 //var_dump($newArr);
 // echo category/name list
 foreach($newArr as $key => $entry){
-	echo '<div style="float:left; width:300px;">';  
-	echo "<div> category:  $key </div>";
-	
-		   echo "<ol>";
+	$html.= "<div style='float:left; width:300px;'>  
+	<div> category:  $key </div> <ol>";
 	foreach($entry as $piece){
-		   echo "<li class='mod-".$piece['module_id']."'>".$piece['name']."</li>"; 
+		   $html.= "<li class='mod-".$piece['module_id']."'>".$piece['name']."</li>"; 
 
 	}
-	
-		   echo "</ol>";
-	echo  '</div>';
+
+		   $html.= "</ol> </div>";
 }
 
-?>
-<div id="nowhere" style="display:none;"></div>
-<br style="clear:both;" />
+$html.= "<div id='nowhere' style='display:none;'></div>
+<br style='clear:both;' />
 
-<input type="hidden" value="" name="order" />
-<button id="submit">Generate form</button>
+<input type='hidden' value='' name='order' />
+<button id='submit'>Generate form</button>
 </form>
 
 </body>
-</html>
+</html>";
+echo $html;
+?>
+
+
